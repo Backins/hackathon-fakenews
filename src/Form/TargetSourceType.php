9fd63d5faf784f43bedfbb0e981363a101f675de
Domange <backinsmail@gmail.com>
2019-03-21 22:57:08 +0100

    Finis back target source

--- src/Form/TargetSourceType.php
+++ src/Form/TargetSourceType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\TargetSource;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,26 @@ class TargetSourceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('domain')
-            ->add('confidenceLevel')
+            ->add('domain', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Nom de domaine'
+                ],
+                'label' => 'Domaine'
+            ])
+            ->add('confidenceLevel', ChoiceType::class, [
+                'choices' => [
+                    'Pas fiable' => 4,
+                    'Peu fiable' => 3,
+                    'Assez fiable' => 2,
+                    'Fiable' => 1,
+                    'Très fiable' => 0,
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Niveau de confiance'
+            ])
         ;
     }
 
